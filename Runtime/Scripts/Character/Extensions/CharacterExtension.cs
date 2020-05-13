6880c400720fa4385b1fa805424c89140d9bccfc
antoripa <antoripa@gmail.com>
2020-05-13 11:04:57 +0200

    Removed All

--- Runtime/Scripts/Character/Extensions/CharacterExtension.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace com.earthshaping.thirdperson.controller
-{
-    public abstract class CharacterExtension : MonoBehaviour
-    {
-        public abstract void UpdateExtension(Character character);
-    }
-}
